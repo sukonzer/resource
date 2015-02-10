1b34293df93b573775eef1dfdfe707162a05c976
sukonzer <sukonzer@gmail.com>
2015-02-10 18:53:05 +0800

    弹窗代码逻辑优化

--- js/widget/dialog.js
+++ js/widget/dialog.js
@@ -34,73 +34,80 @@
 			closeCallback: null
 		}
 		$.extend(this,defaults,option);
-		
-		var _this = this;
-		this.mask.css('opacity',_this.opacity);
-		this.mask.after('<div id="Pop__container__" style="width:'+_this.width+';z-index:'+_this.zIndex+'"></div>');
-		this.container = $('#Pop__container__');
-		this.container.html($.tmpl.render(this.tmpl,this));
-		this.container.css({
-			'position': 'absolute',
-			'left': ($(window).width()-this.container.width())/2,
-			'top': ($(window).height()-this.container.height())/2
-		});
-		
-		this.closePop = function(){
+		this.init();
+	}
+	$.extend(dialog.prototype,{
+		init: function(){
+			var _this = this;
+			this.mask = __maskLayer__;
+			if(!__maskLayer__){
+				__container__.append('<div id="Pop__maskLayer__"></div>');
+				this.mask = __maskLayer__ = $('#Pop__maskLayer__');
+				this.mask.css({'position':'absolute','top':0,'left':0,'background-color':'#000','opacity':_this.opacity,'width':$win.width(),'height':$doc.height()});
+			}else{
+				this.mask.show();
+			}
+			this.mask.after('<div id="Pop__container__" style="width:'+_this.width+';z-index:'+_this.zIndex+'"></div>');
+			this.container = $('#Pop__container__');
+			this.container.html($.tmpl.render(this.tmpl,this));
+			this.container.css({
+				'position': 'absolute',
+				'left': ($win.width()-this.container.width())/2,
+				'top': ($win.height()-this.container.height())/2
+			});
+			
+			$.type(this.loadedCallback) === 'function' && this.loadedCallback.call(this,this.container);
+			//是否开启拖拽
+			if(this.drag){
+				$.drag(this.container,'.hd');
+			}
+			this.addEvent();
+		},
+		//绑定事件
+		addEvent: function(){
+			var _this = this;
+			$doc.on('click','.J_popok',function(e){
+				if(_this.stop){
+					ol.preventDefault(e);
+					ol.stopPropagation(e);
+				}
+				$.type(_this.okCallback) === 'function' && _this.okCallback.call(_this,_this.container,e);
+				if(_this.isHide){
+					_this.closePop();
+				}
+			}).on('click','.J_popcancel',function(e){
+				if(_this.stop){
+					ol.preventDefault(e);
+					ol.stopPropagation(e);
+				}
+				$.type(_this.cancelCallback) === 'function' && _this.cancelCallback.call(_this,_this.container,e);
+				_this.closePop();
+			}).on('click','.J_popclose',function(e){
+				if(_this.stop){
+					ol.stopPropagation(e);
+				}
+				$.type(_this.closeCallback) === 'function' && _this.closeCallback.call(_this,_this.container,e);
+				_this.closePop();
+			});
+			/*//scroll时纠正其位置
+			var initTop = ($win.height() - this.container.height())/2,
+				popTimer;//滚动纠正top定时器
+			$win.on('scroll',function(){
+				if(_this.container.is(':visible')){//显示时才纠正
+					popTimer && clearTimeout(popTimer);
+					popTimer = setTimeout(function(){
+						_this.container.animate({'top':initTop+$win.scrollTop()},500);
+					},200);
+				}
+			});*/
+		},
+		//关闭方法
+		closePop: function(){
 			this.container.remove();
 			this.mask.hide();
 		}
-		$.type(this.loadedCallback) === 'function' && this.loadedCallback.call(this,this.container);
-		//是否开启拖拽
-		if(this.drag){
-			$.drag(this.container,'.hd');
-		}
-		
-		//绑定事件
-		$doc.on('click','.J_popok',function(e){
-			if(_this.stop){
-				ol.preventDefault(e);
-				ol.stopPropagation(e);
-			}
-			$.type(_this.okCallback) === 'function' && _this.okCallback.call(_this,_this.container,e);
-			if(_this.isHide){
-				_this.closePop();
-			}
-		}).on('click','.J_popcancel',function(e){
-			if(_this.stop){
-				ol.preventDefault(e);
-				ol.stopPropagation(e);
-			}
-			$.type(_this.cancelCallback) === 'function' && _this.cancelCallback.call(_this,_this.container,e);
-			_this.closePop();
-		}).on('click','.J_popclose',function(e){
-			if(_this.stop){
-				ol.stopPropagation(e);
-			}
-			$.type(_this.closeCallback) === 'function' && _this.closeCallback.call(_this,_this.container,e);
-			_this.closePop();
-		});
-		/*//scroll时纠正其位置
-		var initTop = ($(window).height()-this.container.height())/2,
-			popTimer;//滚动纠正top定时器
-		$win.on('scroll',function(){
-			if(_this.container.is(':visible')){//显示时才纠正
-				popTimer && clearTimeout(popTimer);
-				popTimer = setTimeout(function(){
-					_this.container.animate({'top':initTop+$win.scrollTop()},500);
-				},200);
-			}
-		});*/
-	}
+	});
 	$.dialog = function(option){
-		if(!__maskLayer__){
-			__container__.append('<div id="Pop__maskLayer__"></div>');
-			__maskLayer__ = $('#Pop__maskLayer__');
-			__maskLayer__.css({'position':'absolute','top':0,'left':0,'background-color':'#000',"width":$win.width(),'height':$doc.height()});
-		}else{
-			__maskLayer__.show();
-		}
-		$.extend(option,{mask:__maskLayer__});
 		return new dialog(option);
 	};
 }(jQuery));
