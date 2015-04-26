a3e41fc7eb7e5f2a73de045285c3f1590ba3811b
sukonzer <sukonzer@gmail.com>
2015-04-26 23:22:15 +0800

    调整list.js文件

--- resource/online/package/action/list.js
+++ resource/online/package/action/list.js
@@ -14,19 +14,27 @@ window.edit = function(data){
 $('.table').on('click','[aria-action=del]', function(){//删除按钮
 	var $this = $(this),
 		$tr = $this.closest('tr');
-	$tr.slideUp(500,function(){
+	$tr.hide(600,function(){
 		$(this).remove();
 	});
 })
 .on('click','[scope=controll]>input',function(){//全反选
 	var $this = $(this),
-		subCk = $('[scope=row]>input');
-	subCk.each(function(i,v){
-			$(v).trigger('click');
+		$tr = $('.table tbody tr');
+	if(this.checked){
+		$tr.each(function(){
+			($(this).find('[scope=row]>input'))[0].checked = true;
+			$(this).addClass('active');
 		});
+	}else{
+		$tr.each(function(){
+			($(this).find('[scope=row]>input'))[0].checked = false;
+			$(this).removeClass('active');
+		});
+	}
 })
 .on('click','[scope=row]>input',function(){//行选
 	var $this = $(this),
 		$tr = $this.closest('tr');
-	$tr[$this[0].checked ? 'addClass' : 'removeClass']('active');
+	$tr[this.checked ? 'addClass' : 'removeClass']('active');
 })
